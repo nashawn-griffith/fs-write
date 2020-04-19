5cf51537cf21e65e63dd8901f8a05861f646741e
nashawn-griffith <nashawn.griffith@gmail.com>
2020-04-19 06:20:57 -0400

    rechanged the files

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const fs = require('fs');
-const fs_write = require('util').promisify(fs.writeFile);
+const fs_read = require('util').promisify(fs.readFile);
 const app = express();
 
 app.get('/write', (req, res) => {
-	fs_write('/tmp/message.txt', 'Testing Mounts')
-		.then(result => {
-			console.log('Written to file');
-			return res.status(200).send('File Updated');
+	// fs_write('/tmp/message.txt', 'Testing Mounts')
+	// 	.then(result => {
+	// 		console.log('Written to file');
+	// 		return res.status(200).send('File Updated');
+	// 	})
+	// 	.catch(err => {
+	// 		console.log(err);
+	// 		return res.status(400).send(err);
+	// 	});
+
+	fs_read('/tmp/message.txt')
+		.then(data => {
+			console.log('Data read: ', data.toString());
+			return res.send(data.toString());
 		})
 		.catch(err => {
+			console.log('Error reading file');
 			console.log(err);
-			return res.status(400).send(err);
+			return res.send(err);
 		});
 });
 
