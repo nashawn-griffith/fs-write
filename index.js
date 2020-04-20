1eba77d4f93086b2e65f894d8af436b6dd0e5611
nashawn-griffith <nashawn.griffith@gmail.com>
2020-04-19 23:26:17 -0400

    modified code

--- index.js
+++ index.js
@@ -3,17 +3,7 @@ const fs = require('fs');
 const fs_read = require('util').promisify(fs.readFile);
 const app = express();
 
-app.get('/write', (req, res) => {
-	// fs_write('/tmp/message.txt', 'Testing Mounts')
-	// 	.then(result => {
-	// 		console.log('Written to file');
-	// 		return res.status(200).send('File Updated');
-	// 	})
-	// 	.catch(err => {
-	// 		console.log(err);
-	// 		return res.status(400).send(err);
-	// 	});
-
+app.get('/read', (req, res) => {
 	fs_read('/tmp/message.txt')
 		.then(data => {
 			console.log('Data read: ', data.toString());
