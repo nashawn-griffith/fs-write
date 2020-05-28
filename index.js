e062a47fa8c4c77bbbbbcbe351c625c0bbd6ea3d
nashawn-griffith <nashawn.griffith@gmail.com>
2020-05-27 21:03:43 -0400

    wip

--- index.js
+++ index.js
@@ -2,41 +2,51 @@ const fs = require('fs');
 const express = require('express');
 const fs_write = require('util').promisify(fs.writeFile);
 const fs_read = require('util').promisify(fs.readFile);
+const originalConsoleError = console.error;
+const originalConsoleLog = console.log;
+console.log = (...args) => originalConsoleLog('LOG:', args);
+console.error = (...args) => originalConsoleError('ERROR:', args);
 
 const app = express();
 
 app.get('/monitor', (req, res) => {
-	return res.send('Write server is OK');
+	return res.send('server is OK');
 });
 
-app.get('/write/:text', (req, res) => {
-	console.log('Write is starting');
-	const {text} = req.params;
-	fs_write('/tmp/message.txt', text)
-		.then(result => {
-			console.log('Written to file');
-			return res.send('data written');
-		})
-		.catch(err => {
-			console.log(err);
-			return res.send(err);
-		});
+app.get('/', (req, res) => {
+	console.log('Good', 'News');
+	console.error('Bad', 'news');
+	return res.send('DONE');
 });
 
-app.get('/read', (req, res) => {
-	console.log('Reading from original');
-	fs_read('/tmp/message.txt')
-		.then(data => {
-			console.log('Data read: ', data.toString());
-			return res.send(data.toString());
-		})
-		.catch(err => {
-			console.log('Error reading file');
-			console.log(err);
-			return res.send(err);
-		});
-});
+// app.get('/write/:text', (req, res) => {
+// 	console.log('Write is starting');
+// 	const {text} = req.params;
+// 	fs_write('/tmp/message.txt', text)
+// 		.then(result => {
+// 			console.log('Written to file');
+// 			return res.send('data written');
+// 		})
+// 		.catch(err => {
+// 			console.log(err);
+// 			return res.send(err);
+// 		});
+// });
+
+// app.get('/read', (req, res) => {
+// 	console.log('Reading from original');
+// 	fs_read('/tmp/message.txt')
+// 		.then(data => {
+// 			console.log('Data read: ', data.toString());
+// 			return res.send(data.toString());
+// 		})
+// 		.catch(err => {
+// 			console.log('Error reading file');
+// 			console.log(err);
+// 			return res.send(err);
+// 		});
+// });
 
 app.listen(5000, () => {
-	console.log('Write server running');
+	console.log(`Server running on port 5000`);
 });
